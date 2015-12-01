c1ef30161d754c3330714baef4f70f3e883fd401
vikey89 <vincenzo.provenza89@gmail.com>
2015-12-01 18:10:23 +0100

    first commit

---
+++ www/js/controllers.js
@@ -0,0 +1,93 @@
+angular.module('starter.controllers', [])
+
+.controller('AppCtrl', function($scope, $ionicModal, $timeout) {
+
+  // With the new view caching in Ionic, Controllers are only called
+  // when they are recreated or on app start, instead of every page change.
+  // To listen for when this page is active (for example, to refresh data),
+  // listen for the $ionicView.enter event:
+  //$scope.$on('$ionicView.enter', function(e) {
+  //});
+
+  // Form data for the login modal
+  $scope.loginData = {};
+
+  // Create the login modal that we will use later
+  $ionicModal.fromTemplateUrl('templates/login.html', {
+    scope: $scope
+  }).then(function(modal) {
+    $scope.modal = modal;
+  });
+
+  // Triggered in the login modal to close it
+  $scope.closeLogin = function() {
+    $scope.modal.hide();
+  };
+
+  // Open the login modal
+  $scope.login = function() {
+    $scope.modal.show();
+  };
+
+  // Perform the login action when the user submits the login form
+  $scope.doLogin = function() {
+    console.log('Doing login', $scope.loginData);
+
+    // Simulate a login delay. Remove this and replace with your login
+    // code if using a login system
+    $timeout(function() {
+      $scope.closeLogin();
+    }, 1000);
+  };
+})
+
+.controller('PlaylistsCtrl', function($scope, $ionicPlatform, $cordovaClipboard) {
+  $scope.playlists = [
+    { title: 'Reggae', id: 1 },
+    { title: 'Chill', id: 2 },
+    { title: 'Dubstep', id: 3 },
+    { title: 'Indie', id: 4 },
+    { title: 'Rap', id: 5 },
+    { title: 'Cowbell', id: 6 }
+  ];
+
+  $ionicPlatform.ready(function() {
+
+    Ionic.io();
+
+    var push = new Ionic.Push({
+      "debug": true,
+      "onNotification": function(notification) {
+        var payload = notification.payload;
+        console.log(notification, payload);
+      },
+      "onRegister": function(data) {
+        console.log(data.token);
+      },
+      "pluginConfig": {
+        "android": {
+          "iconColor": "#343434"
+        }
+      }
+    });
+
+    var user = Ionic.User.current();
+    var callback = function(pushToken) {
+      $scope.token = pushToken.token;
+      user.addPushToken(pushToken);
+      user.save(); // you NEED to call a save after you add the token
+    }
+
+    push.register(callback);
+
+  });
+
+  $scope.toCopy = function() {
+    console.log($scope.token);
+    $cordovaClipboard.copy($scope.token);
+  }
+
+})
+
+.controller('PlaylistCtrl', function($scope, $stateParams) {
+});
