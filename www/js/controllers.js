ec9ac7809e72bd8681058b51a038f5dce92f44c0
vikey89 <vincenzo.provenza89@gmail.com>
2015-12-01 22:50:48 +0100

    wip login

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,93 +1,104 @@
 angular.module('starter.controllers', [])
 
-.controller('AppCtrl', function($scope, $ionicModal, $timeout) {
+    .controller('AppCtrl', function ($scope, $ionicModal, $timeout, LoginService, $ionicPopup, $state) {
 
-  // With the new view caching in Ionic, Controllers are only called
-  // when they are recreated or on app start, instead of every page change.
-  // To listen for when this page is active (for example, to refresh data),
-  // listen for the $ionicView.enter event:
-  //$scope.$on('$ionicView.enter', function(e) {
-  //});
+        // With the new view caching in Ionic, Controllers are only called
+        // when they are recreated or on app start, instead of every page change.
+        // To listen for when this page is active (for example, to refresh data),
+        // listen for the $ionicView.enter event:
+        //$scope.$on('$ionicView.enter', function(e) {
+        //});
 
-  // Form data for the login modal
-  $scope.loginData = {};
+        // Form data for the login modal
+        $scope.loginData = {};
 
-  // Create the login modal that we will use later
-  $ionicModal.fromTemplateUrl('templates/login.html', {
-    scope: $scope
-  }).then(function(modal) {
-    $scope.modal = modal;
-  });
+        // Create the login modal that we will use later
+        $ionicModal.fromTemplateUrl('templates/login.html', {
+            scope: $scope
+        }).then(function (modal) {
+            $scope.modal = modal;
+        });
 
-  // Triggered in the login modal to close it
-  $scope.closeLogin = function() {
-    $scope.modal.hide();
-  };
+        // Triggered in the login modal to close it
+        $scope.closeLogin = function () {
+            $scope.modal.hide();
+        };
 
-  // Open the login modal
-  $scope.login = function() {
-    $scope.modal.show();
-  };
+        // Open the login modal
+        $scope.login = function () {
+            $scope.modal.show();
+        };
 
-  // Perform the login action when the user submits the login form
-  $scope.doLogin = function() {
-    console.log('Doing login', $scope.loginData);
+        // Perform the login action when the user submits the login form
+        $scope.data = {};
 
-    // Simulate a login delay. Remove this and replace with your login
-    // code if using a login system
-    $timeout(function() {
-      $scope.closeLogin();
-    }, 1000);
-  };
-})
-
-.controller('PlaylistsCtrl', function($scope, $ionicPlatform, $cordovaClipboard) {
-  $scope.playlists = [
-    { title: 'Reggae', id: 1 },
-    { title: 'Chill', id: 2 },
-    { title: 'Dubstep', id: 3 },
-    { title: 'Indie', id: 4 },
-    { title: 'Rap', id: 5 },
-    { title: 'Cowbell', id: 6 }
-  ];
-
-  $ionicPlatform.ready(function() {
-
-    Ionic.io();
-
-    var push = new Ionic.Push({
-      "debug": true,
-      "onNotification": function(notification) {
-        var payload = notification.payload;
-        console.log(notification, payload);
-      },
-      "onRegister": function(data) {
-        console.log(data.token);
-      },
-      "pluginConfig": {
-        "android": {
-          "iconColor": "#343434"
+        $scope.doLogin = function() {
+            LoginService.loginUser($scope.loginData.username, $scope.loginData.password).success(function(data) {
+                $state.go('app.search');
+                $timeout(function() {
+                    $scope.closeLogin();
+                }, 1000);
+            }).error(function(data) {
+                var alertPopup = $ionicPopup.alert({
+                    title: 'Login failed!',
+                    template: 'Please check your credentials!'
+                });
+            });
         }
-      }
+    })
+
+    .controller('PlaylistsCtrl', function ($scope, $ionicPlatform, $cordovaClipboard) {
+        $scope.playlists = [
+            {title: 'Reggae', id: 1},
+            {title: 'Chill', id: 2},
+            {title: 'Dubstep', id: 3},
+            {title: 'Indie', id: 4},
+            {title: 'Rap', id: 5},
+            {title: 'Cowbell', id: 6}
+        ];
+
+        $ionicPlatform.ready(function () {
+
+            Ionic.io();
+
+            var push = new Ionic.Push({
+                "debug": true,
+                "onNotification": function (notification) {
+                    var payload = notification.payload;
+                    console.log(notification, payload);
+                },
+                "onRegister": function (data) {
+                    console.log(data.token);
+                },
+                "pluginConfig": {
+                    "android": {
+                        "iconColor": "#343434"
+                    }
+                }
+            });
+
+            var user = Ionic.User.current();
+            var callback = function (pushToken) {
+                $scope.token = pushToken.token;
+
+                if (!user.id) {
+                    user.id = Ionic.User.anonymousId();
+                }
+
+                user.addPushToken(pushToken);
+                user.save(); // you NEED to call a save after you add the token
+            }
+
+            push.register(callback);
+
+        });
+
+        $scope.toCopy = function () {
+            console.log($scope.token);
+            $cordovaClipboard.copy($scope.token);
+        }
+
+    })
+
+    .controller('PlaylistCtrl', function ($scope, $stateParams) {
     });
-
-    var user = Ionic.User.current();
-    var callback = function(pushToken) {
-      $scope.token = pushToken.token;
-      user.addPushToken(pushToken);
-      user.save(); // you NEED to call a save after you add the token
-    }
-
-    push.register(callback);
-
-  });
-
-  $scope.toCopy = function() {
-    console.log($scope.token);
-    $cordovaClipboard.copy($scope.token);
-  }
-
-})
-
-.controller('PlaylistCtrl', function($scope, $stateParams) {
-});
