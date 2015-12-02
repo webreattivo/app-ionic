4048d1da44514725a8a1bff62c5f4175a77f9d3f
vikey89 <vincenzo.provenza89@gmail.com>
2015-12-02 16:45:40 +0100

    refactoring app

--- www/js/app.js
+++ www/js/app.js
@@ -4,70 +4,64 @@
 // 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
 // the 2nd parameter is an array of 'requires'
 // 'starter.controllers' is found in controllers.js
-angular.module('starter', ['ionic','ionic.service.core', 'starter.controllers', 'starter.servicies', 'ngCordova'])
+angular.module('starter', ['ionic', 'ionic.service.core', 'starter.controllers', 'starter.servicies', 'ngCordova'])
 
-.run(function($ionicPlatform) {
-  $ionicPlatform.ready(function() {
-    // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
-    // for form inputs)
-    if (window.cordova && window.cordova.plugins.Keyboard) {
-      cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
-      cordova.plugins.Keyboard.disableScroll(true);
+    .run(function ($ionicPlatform) {
+        $ionicPlatform.ready(function () {
+            // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
+            // for form inputs)
+            if (window.cordova && window.cordova.plugins.Keyboard) {
+                cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
+                cordova.plugins.Keyboard.disableScroll(true);
 
-    }
-    if (window.StatusBar) {
-      // org.apache.cordova.statusbar required
-      StatusBar.styleDefault();
-    }
-  });
-})
-
-.config(function($stateProvider, $urlRouterProvider) {
-  $stateProvider
-
-    .state('app', {
-    url: '/app',
-    abstract: true,
-    templateUrl: 'templates/menu.html',
-    controller: 'AppCtrl'
-  })
-
-  .state('app.search', {
-    url: '/search',
-    views: {
-      'menuContent': {
-        templateUrl: 'templates/search.html'
-      }
-    }
-  })
-
-  .state('app.browse', {
-      url: '/browse',
-      views: {
-        'menuContent': {
-          templateUrl: 'templates/browse.html'
-        }
-      }
-    })
-    .state('app.playlists', {
-      url: '/playlists',
-      views: {
-        'menuContent': {
-          templateUrl: 'templates/playlists.html',
-          controller: 'PlaylistsCtrl'
-        }
-      }
+            }
+            if (window.StatusBar) {
+                // org.apache.cordova.statusbar required
+                StatusBar.styleDefault();
+            }
+        });
     })
 
-  .state('app.single', {
-    url: '/playlists/:playlistId',
-    views: {
-      'menuContent': {
-        templateUrl: 'templates/playlist.html',
-        controller: 'PlaylistCtrl'
-      }
-    }
-  });
-  // if none of the above states are matched, use this as the fallback
-  $urlRouterProvider.otherwise('/app/playlists');
-});
+    .config(function ($stateProvider, $urlRouterProvider) {
+        $stateProvider
+
+            .state('app', {
+                url: '/app',
+                abstract: true,
+                templateUrl: 'templates/menu.html',
+                controller: 'AppCtrl'
+            })
+
+            .state('app.login', {
+                url: '/login',
+                views: {
+                    'menuContent': {
+                        templateUrl: 'templates/login.html',
+                        controller: 'LoginCtrl'
+                    }
+                }
+            })
+
+            .state('app.dashboard', {
+                url: '/dashboard',
+                views: {
+                    'menuContent': {
+                        templateUrl: 'templates/dashboard.html',
+                        controller: 'DashCtrl'
+                    }
+                }
+            })
+
+            .state('app.push-notification', {
+                url: '/push-notification',
+                views: {
+                    'menuContent': {
+                        templateUrl: 'templates/push-notification.html',
+                        controller: 'PushCtrl'
+                    }
+                }
+            });
+
+        // if none of the above states are matched, use this as the fallback
+        $urlRouterProvider.otherwise('/app/login');
+    });
--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,66 +1,46 @@
 angular.module('starter.controllers', [])
 
-    .controller('AppCtrl', function ($scope, $ionicModal, $timeout, LoginService, $ionicPopup, $state) {
+    .controller('AppCtrl', function ($scope, $ionicModal, $timeout, $ionicPopup, $state) {
 
-        // With the new view caching in Ionic, Controllers are only called
-        // when they are recreated or on app start, instead of every page change.
-        // To listen for when this page is active (for example, to refresh data),
-        // listen for the $ionicView.enter event:
-        //$scope.$on('$ionicView.enter', function(e) {
-        //});
+    })
 
-        // Form data for the login modal
+    .controller('LoginCtrl', function ($scope, $state, $ionicPopup, LoginService, $cordovaOauth) {
+
         $scope.loginData = {};
-
-        // Create the login modal that we will use later
-        $ionicModal.fromTemplateUrl('templates/login.html', {
-            scope: $scope
-        }).then(function (modal) {
-            $scope.modal = modal;
-        });
-
-        // Triggered in the login modal to close it
-        $scope.closeLogin = function () {
-            $scope.modal.hide();
-        };
-
-        // Open the login modal
-        $scope.login = function () {
-            $scope.modal.show();
-        };
-
-        // Perform the login action when the user submits the login form
-        $scope.data = {};
 
         $scope.doLogin = function() {
             LoginService.loginUser($scope.loginData.username, $scope.loginData.password).success(function(data) {
-                $state.go('app.search');
-                $timeout(function() {
-                    $scope.closeLogin();
-                }, 1000);
+                $state.go('app.dashboard');
             }).error(function(data) {
-                var alertPopup = $ionicPopup.alert({
+                $ionicPopup.alert({
                     title: 'Login failed!',
                     template: 'Please check your credentials!'
                 });
             });
-        }
+        };
+
+        $scope.facebookLogin = function() {
+            $cordovaOauth.facebook("1520497348260580", ["email"]).then(function(result) {
+                console.log("success");
+                console.log(result);
+                $state.go('app.dashboard');
+            }, function(error) {
+                // error
+                console.log("error");
+                console.log(error);
+            });
+        };
     })
 
-    .controller('PlaylistsCtrl', function ($scope, $ionicPlatform, $cordovaClipboard, $cordovaOauth) {
-        $scope.playlists = [
-            {title: 'Reggae', id: 1},
-            {title: 'Chill', id: 2},
-            {title: 'Dubstep', id: 3},
-            {title: 'Indie', id: 4},
-            {title: 'Rap', id: 5},
-            {title: 'Cowbell', id: 6}
-        ];
+    .controller('DashCtrl', function($scope) {
+
+    })
+
+    .controller('PushCtrl', function($scope, $ionicPlatform, $cordovaClipboard) {
 
         $ionicPlatform.ready(function () {
 
             Ionic.io();
-
             var push = new Ionic.Push({
                 "debug": true,
                 "onNotification": function (notification) {
@@ -86,30 +66,14 @@ angular.module('starter.controllers', [])
                 }
 
                 user.addPushToken(pushToken);
-                user.save(); // you NEED to call a save after you add the token
-            }
+                user.save();
+            };
 
             push.register(callback);
-
         });
 
         $scope.toCopy = function () {
             console.log($scope.token);
             $cordovaClipboard.copy($scope.token);
         };
-
-        $scope.facebookLogin = function() {
-            $cordovaOauth.facebook("1520497348260580", ["email"]).then(function(result) {
-                console.log("success");
-                console.log(result);
-            }, function(error) {
-                // error
-                console.log("error");
-                console.log(error);
-            });
-        };
-
-    })
-
-    .controller('PlaylistCtrl', function ($scope, $stateParams) {
     });
